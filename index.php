460c338d6635662b63857ae8bde2a23c1fc2d291
johnrak11 <vorak.yun@student.passerellesnumeriques.org>
2023-01-23 13:47:13 +0700

    set up framwork tw

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-require 'utils/url.php';
-require 'database/database.php';
-require 'router.php';
+require_once('views/partials/head.php');
 
-
+?>
+<h1 class="text-3xl underline ">
+    Hello world!
+</h1>
