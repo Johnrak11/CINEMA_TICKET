5bc169a7dd66d4ee5c94cafda019d3edcc52bcf8
johnrak11 <vorak.yun@student.passerellesnumeriques.org>
2023-01-24 09:59:23 +0700

    [CT-22] Validate backend register form

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once('views/partials/head.php');
 require_once('views/formes/register_form.view.php');
+require_once('views/partials/footer.php');
+
+
 
 ?>
--- views/partials/head.php
+++ views/partials/head.php
@@ -5,4 +5,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="flex justify-center items-center">
