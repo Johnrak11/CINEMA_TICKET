4e35d8805084b78a237cb025b71dadaf6e3f875d
YOEM.SREYYOER <YOEM.SREYYOER@PNC.LAN>
2023-01-24 15:23:21 +0700

    [CT-19] Create view form login dialog

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 require_once('views/partials/head.php');
+require_once('views/forms/login_form.view.php');
+
+
 
 ?>
-<h1 class="text-3xl underline ">
+<!-- <h1 class="text-3xl underline ">
     Hello world!
-</h1>
+</h1> -->
