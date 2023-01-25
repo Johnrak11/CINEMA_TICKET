d981e66195910fd09b1d786efa22499e5bb090ae
johnrak11 <vorak.yun@student.passerellesnumeriques.org>
2023-01-25 12:31:49 +0700

    [CT-31] create profile view

--- views/js/main.js
+++ views/js/main.js
@@ -1,11 +1,23 @@
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }))
+let domDropdown = document.querySelector('#hidden')
+let domProfile = document.querySelector('#profile-user')
+domProfile.addEventListener("click", (e) => {
+    domDropdown.style.display = "block";
+    console.log(e.target)
+
+});
+domDropdown.addEventListener("click", (e) => {
+    if (e.target.id === 'background-hide'){
+        domDropdown.style.display = "none";
+    }
 })
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,39 +1,75 @@
-<nav class="px-2 sm:px-4 py-2.5 dark:bg-black fixed w-full z-20 top-0 left-0 border-b dark:border-black">
+<?php
+session_start();
+// $_SESSION['email'] = "virak";
+// $_SESSION['password'] = "password";
+?>
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="#" class="flex items-center">
-            <!-- <img src="#" class="h-6 mr-3 sm:h-9"> -->
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"> CINEMA </span>
+        <a href="https://flowbite.com/" class="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CINEMA</span>
         </a>
-        <div class=" flex md:order-2 ">
-            <a href="login" class="register text-white hover:text-red-700  font-medium  text-xl px-5 py-2.5 text-center mr-3 md:mr-0" id="nav-register">
-                LOGIN
-            </a>
-        </div>
-        <div class="hamburger order-2 or">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </div>
-        <div class="items-center justify-between md:flex  md:order-1">
-            <div class=" items-center  justify-between">
-                <ul class="nav-menu flex md:flex-row md:space-x-8 md:text-xl md:font-medium ">
-                    <li class="nav-item login" >
-                        <a href="login" class="nav-link py-1 dark:text-white hover:text-red-700" id="nav-register">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-1 dark:text-white hover:text-red-700">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-1 dark:text-white hover:text-red-700">Show</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-1 dark:text-white hover:text-red-700">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-1 dark:text-white hover:text-red-700">Contact</a>
-                    </li>
-                </ul>
+        <div class="flex items-center md:order-2">
+            <?php
+            if (isset($_SESSION['email']) and isset($_SESSION['password'])) {
+            ?>
+                <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <span class="sr-only">Open user menu</span>
+                    <img id="profile-user" class="w-8 h-8 rounded-full" src='views/images/user_account.png' alt="user photo">
+                </button>
+
+            <?php
+            } else {
+            ?>
+                <a href="/login"><button type="button" class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Login</button></a>
+            <?php
+            }
+            ?>
+            <!-- Dropdown menu -->
+            <div id="hidden" class="hidden">
+                <div id="background-hide"></div>
+                <div id="dropdown-menu" class="z-50  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-2" aria-labelledby="user-menu-button">
+                        <li>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile Detail</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Purchese</a>
+                        </li>
+                        <li>
+                            <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+            <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Show</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
