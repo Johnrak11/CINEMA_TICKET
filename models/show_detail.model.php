1a9f7fe2ce528a85e57c6de9bf97dd40861c1614
thanachhoeun <chhoeunthana1512002@gmail.com>
2023-01-27 08:47:41 +0700

    [CT-42] show_detail

---
+++ models/show_detail.model.php
@@ -0,0 +1,10 @@
+<?php
+function showDetail( int $id) :array
+{
+    global $connection;
+    $statement = $connection->prepare('SELECT * FROM shows WHERE id = :id'); 
+    $statement -> execute([
+        ":id" => $id,
+    ]);
+    return $statement ->fetch();
+}  
