29cfaa44fcfbe51a544ccb01a20b734d9e856e27
johnrak11 <vorak.yun@student.passerellesnumeriques.org>
2023-01-27 08:28:58 +0700

    [CT-37] create detail page view

--- views/pages/detail.view.php
+++ views/pages/detail.view.php
@@ -1,16 +1,26 @@
-<div class="mt-[5%]">
-    <div class="flex justify-between">
-        <div class="text-3xl ml-[10%]">
-            <br><br><br>
-            <h1>Titlle :</h1><br>
-            <h1>Time :</h1><br>
-            <h1>Venue :</h1><br>
-            <h1>Author :</h1><br>
+<?php
+require_once('views/partials/head.php');
+require_once('views/partials/nav.php');
+?>
+<div class="mt-[8%] flex flex-col">
+    <div class="flex justify-between flex-2 text-white">
+        <div class="text-3xl ml-[10%] " style="flex:2;">
+            <h1 class="text-2xl mt-3">TITLE <i class="fa fa-film ml-2" style="font-size:24px"></i> <span class="ml-[11%]"> Avatar-2: The way of wather</span></h1>
+            <h1 class="text-2xl mt-3">VENUE<i class='fas fa-map-marker-alt ml-2' style='font-size:24px'></i> <span class="ml-[10%]"> Kep, cambodia</span></h1>
+            <h1 class="text-2xl mt-3">AUTHOR <i class='fas fa-book ml-2' style='font-size:24px'></i> <span class="ml-10"> Rthana</span></h1>
+            <h1 class="text-2xl mt-3">DESCRIPTION </h1>
+            <p class="text-xl mt-2">dolor sit amet consectetur adipisicing elit. Impedit, unde voluptates. Nam ut esse unde, expedita eius eos, vel architecto, totam dolorum modi odio non error accusantium sunt voluptas quaerat? </p>
         </div>
-        <div class="">
-            <br><br><br>
-            <img src="https://noguiltdisney.com/wp-content/uploads/2022/12/AVATAR_-THE-WAY-OF-WATER-poster-scaled.jpg.webp" alt="" class="w-[50%] ml-[20%]"><br>
-            <button class="ml-[20%] bg-red-600 py-3 px-3 text-white font-bold uppercase text-xs rounded hover:bg-red-900 hover:text-white-800 w-[50%]">Book Now</button>
+        <div class="flex justify-center flex-col items-end mr-[10%] " style="flex:1;">
+            <img id="right-card-container" src="https://noguiltdisney.com/wp-content/uploads/2022/12/AVATAR_-THE-WAY-OF-WATER-poster-scaled.jpg.webp" alt="" class="w-[70%] rounded-xl"><br>
+            <button class=" bg-red-600 py-3 px-3 text-white font-bold uppercase text-xs hover:bg-red-900 hover:text-white-800 w-[45%] mr-[13%] rounded-xl">Book Now</button>
         </div>
     </div>
+    <h1 class="flex-1 text-3xl ml-[10%] text-white">Trailler</h1>
+    <div class="trailler-container flex-3  py-5 flex justify-center align-center flex-1">
+        <iframe src="https://www.youtube.com/embed/3dsNr2uFUhI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
 </div>
+<?php
+require_once('views/partials/footer.php');
+?>
