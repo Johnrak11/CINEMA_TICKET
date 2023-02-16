7b1d148f3acced4d8673e5ae3b6d4f69334d3e0d
thanachhoeun <chhoeunthana1512002@gmail.com>
2023-02-16 09:15:55 +0700

    [CT-95] create page edit show

--- index.php
+++ index.php
@@ -3,5 +3,4 @@ require_once("database/database.php");
 require_once("utils/url.php");
 require_once("models/role.model.php");
 require_once('router.php');
-require_once('views/pages/booking/payment.view.php');
 ?>
