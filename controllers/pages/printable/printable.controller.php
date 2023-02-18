e47eb4a8b221fc1664f35983dc1f4d3dbe5ac0af
YOEM.SREYYOER <YOEM.SREYYOER@PNC.LAN>
2023-02-18 16:33:24 +0700

    [CT-101]VIEW - create printable interface

--- controllers/pages/printable/printable.controller.php
+++ controllers/pages/printable/printable.controller.php
@@ -1,3 +1,3 @@
 <?php
-require_once('views/pages/printable/order_show.view.php')
+require_once('views/pages/printable/printable.view.php')
 ?>
---
+++ views/pages/about/about.view.php
@@ -0,0 +1,13 @@
+<?php
+require_once("views/partials/head.php");
+require_once("views/partials/nav.php");
+?>
+<div class="container">
+                    
+
+
+</div>
+<?php
+
+// require_once("views/partials/footer.php");
+?>
